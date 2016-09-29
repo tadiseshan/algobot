566902930c5ac8d598dd964faae73abe9d4b28c6
taraadiseshan <tara.adiseshan@gmail.com>
2016-09-29 11:56:45 -0400

    deleting secrets

--- headlines.py
+++ headlines.py
@@ -10,23 +10,6 @@ import feedparser
 def get():
 	query = "algorithm"
 
-	#query nyt api
-	# nyt_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=" + NYT_KEY
-	# response = requests.get(nyt_url)
-	# nyt_data = response.json()
-	# nyt_docs = nyt_data["response"]["docs"]
-	# headlines = []
-	# for i in nyt_docs:
-	# 	headlines.append(i['headline']['main'])
-
-	# #query guardian api
-	# g_url = "http://content.guardianapis.com/search?q=" + query + "&api-key=" + GUARDIAN_KEY
-	# g_response = requests.get(g_url)
-	# g_data = g_response.json()
-	# g_docs = g_data["response"]["results"]
-	# for j in g_docs:
-	# 	headlines.append(j['webTitle'])
-
 	def parseRSS(rss_url):
 		return(feedparser.parse(rss_url))
 
@@ -35,10 +18,7 @@ def get():
 		feed = parseRSS(rss_url)
 		for item in feed['items']:
 			headlines.append(item['title'])
-
 		return(headlines)
-
-
 
 	tweet(getHeadlines('https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q=algorithm&output=rss'))
 
